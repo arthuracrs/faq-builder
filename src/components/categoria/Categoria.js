9c20aee53ec46bb9f68351d87c487178c5152401
Arthur Calebe Ribeiro Soares <arthur.soares@pampa.compasso>
2022-08-19 08:16:11 -0300

    sera?

--- src/components/categoria/Categoria.js
+++ src/components/categoria/Categoria.js
@@ -6,18 +6,58 @@ import { useState } from 'react';
 import { Pergunta } from '../pergunta/Pergunta';
 
 export function Categoria() {
-    const [perguntas, setPerguntas] = useState([{ id: crypto.randomUUID() }]);
-    const [colunas, setColunas] = useState([
-        { id: 'fom1', data: [{ id: '1a' }, { id: '2a' }] },
-        { id: 'fom2', data: [{ id: '2b' }] }
-    ]);
+    const [perguntas, setPerguntas] = useState({
+        col1: [{ id: crypto.randomUUID() }],
+        col2: [{ id: crypto.randomUUID() }]
+    });
 
     const [color, setColor] = useState('white')
 
-    function handleAddPergunta(event) {
+    function Coluna({ droppableId }) {
+        return (
+            <div>
+                <button onClick={() => handleAddPergunta(droppableId)}>Nova Pergunta</button>
+                <Droppable droppableId={droppableId} key={droppableId}>
+                    {(provided) => (
+                        <div className='col'
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                        >
+                            {perguntas[droppableId].map((item, index) => (
+                                <Peritem key={item.id} item={item} col={droppableId} index={index} />
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </div>
+        )
+    }
 
+    function Peritem({ item, index, col }) {
+        return (
+            <Draggable key={item.id} draggableId={item.id} index={index}>
+                {(provided) => (
+                    <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
 
-        setPerguntas([...perguntas, { id: crypto.randomUUID() }]);
+                    >
+                        <Pergunta idPergunta={item.id} deleteHandler={() => deletePergunta(item.id, col)} />
+                    </div>
+                )}
+
+            </Draggable>
+        )
+    }
+
+    function handleAddPergunta(col) {
+        console.log(perguntas[col])
+        setPerguntas({
+            ...perguntas,
+            [col]: [...perguntas[col], { id: crypto.randomUUID() }]
+        });
     }
 
     function handleOnChange(event) {
@@ -27,78 +67,72 @@ export function Categoria() {
             setColor('white')
     }
 
-    function deletePergunta(id) {
-        setPerguntas(perguntas.filter(x => x.id !== id))
+    function copyObj(obj) {
+        return JSON.parse(JSON.stringify(obj))
     }
 
-
-    function findColuna(id) {
-        return colunas.find(coluna => coluna.id === id)
+    function deletePergunta(id, col) {
+        console.log(perguntas[col])
+        let newPerguntas = copyObj(perguntas)
+        newPerguntas[col] = newPerguntas[col].filter(x => x.id !== id)
+        setPerguntas(newPerguntas)
     }
+
+    const reorder = (list, startIndex, endIndex) => {
+        const result = Array.from(list);
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+
+        return result;
+    };
 
     function onDragEnd(result) {
-        // dropped outside the list
-        if (!result.destination) {
+        const { source, destination } = result;
+        if (!destination) {
             return;
         }
 
-        if (result.source.droppableId == result.destination.droppableId) {
+        if (destination.droppableId === source.droppableId) {
+            if (destination.index === source.index) {
+                return;
+            }
 
+            const widgets = reorder(
+                perguntas[source.droppableId],
+                source.index,
+                destination.index
+            );
+
+            setPerguntas({
+                ...perguntas,
+                [source.droppableId]: widgets
+            });
+        } else {
+            let tempPerguntas = copyObj(perguntas)
+
+            const startColumn = [...tempPerguntas[source.droppableId]];
+            const finishColumn = [...tempPerguntas[destination.droppableId]];
+
+            const [removed] = startColumn.splice(source.index, 1);
+            finishColumn.splice(destination.index, 0, removed);
+
+            tempPerguntas[source.droppableId] = startColumn
+            tempPerguntas[destination.droppableId] = finishColumn
+
+            setPerguntas(tempPerguntas)
         }
-        else {
-            // console.log(findColuna(result.source.index))
-            console.log(result)
-            const colunaSource = findColuna(result.source.droppableId).data
-            console.log(colunaSource)
-            const [perguntaRemovedFromSource] = colunaSource.splice(result.source.index, 1);
-            console.log(colunaSource)
-            console.log(perguntaRemovedFromSource)
-            const colunaDestination = findColuna(result.destination.droppableId).data
-
-            colunaDestination.splice(result.destination.index, 0, perguntaRemovedFromSource);
-
-            console.log(colunaDestination)
-        }
-
-        // console.log(result.source.index)
-        // console.log(result);
-
-        // setPerguntas(tempPerguntas)
     }
 
     return (
         <>
             <h1 onInput={handleOnChange} style={{ backgroundColor: color }} suppressContentEditableWarning={true} contentEditable="true">Perguntas frequentes</h1>
-            <button onClick={handleAddPergunta}>Nova Pergunta</button>
-            <div className='fom'>
+
+            <div >
                 <DragDropContext onDragEnd={onDragEnd}>
-                    {colunas.map((coluna, index) => (
-                        <Droppable droppableId={coluna.id} key={coluna.id}>
-                            {(provided) => (
-                                <div
-                                    {...provided.droppableProps}
-                                    ref={provided.innerRef}
-                                >
-                                    {coluna.data.map((item, index) => (
-                                        <Draggable key={item.id} draggableId={item.id} index={index}>
-                                            {(provided) => (
-                                                <div
-                                                    ref={provided.innerRef}
-                                                    {...provided.draggableProps}
-                                                    {...provided.dragHandleProps}
-
-                                                >
-                                                    <Pergunta idPergunta={item.id} deleteHandler={deletePergunta} />
-                                                </div>
-                                            )}
-
-                                        </Draggable>
-                                    ))}
-                                    {provided.placeholder}
-                                </div>
-                            )}
-                        </Droppable>
-                    ))}
+                    <div className='fom'>
+                        <Coluna droppableId={'col1'} key={'col1'} />
+                        <Coluna droppableId={'col2'} key={'col2'} />
+                    </div>
                 </DragDropContext>
             </div>
         </>
